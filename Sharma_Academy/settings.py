25ee01e68cebbb9687b476ce0b5ce5ddbd89ba9e
prnvlohar <prnvlohar@gmail.com>
2024-03-06 12:40:53 +0000

    make changes in settings.py

--- Sharma_Academy/settings.py
+++ Sharma_Academy/settings.py
@@ -26,16 +26,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'afsarbabu.in',"*"]
-
 SESSION_COOKIE_SECURE = True  # Use secure cookies for session
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 CSRF_TRUSTED_ORIGINS = ['https://afsarbabu.in']
-
-
 
 # Application definition
 
