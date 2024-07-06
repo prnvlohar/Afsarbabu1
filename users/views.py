b14a414413ae57e5a12bb4172ca3bb790c3e4525
nitin7585pelago <nitin.verma@pelago.co>
2024-07-06 18:48:40 +0530

    changed index_url

--- users/views.py
+++ users/views.py
@@ -37,7 +37,7 @@ class RegisterView(View):
 
     def get(self, request, *args, **kwargs):
         if request.user.is_authenticated:
-            return redirect("/index?login=True")
+            return redirect(reverse('index')+"?login=True")
         return render(request, self.template_name, {"form": self.form_class()})
 
     def post(self, request):
@@ -89,7 +89,7 @@ class InstituteRegisterView(View):
 
     def get(self, request, *args, **kwargs):
         if request.user.is_authenticated:
-            return redirect("/index?login=True")
+            return redirect(reverse('index')+"?login=True")
         return render(request, self.template_name, {"form": self.form_class()})
 
     def post(self, request, *args, **kwargs):
