0e15259afcdc89c2e26438ab01f25854fe4bd0f6
prnvlohar <prnvlohar@gmail.com>
2024-07-13 17:00:25 +0530

    added changes

--- assessment/views.py
+++ assessment/views.py
@@ -1,17 +1,20 @@
 from typing import Any
 from django.contrib import messages
 from django.db.models import Avg
+from django.db.models.query import QuerySet
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from django.views import View
+from django.core import serializers
 from django.views.generic import (CreateView, DeleteView, DetailView, ListView,
                                   UpdateView)
 from django.shortcuts import get_object_or_404
 from assessment.forms import (AssessmentForm, QuestionForm, RatingForm, SubtopicForm, TopicForm, ExamForm)
 from assessment.models import ( Assessment, Question,
-                               Rating, PassFailStatus, Topic, Subtopic, Exams)
-from assessment.utils import send_email_with_marks
+                               Rating, PassFailStatus, Topic, Subtopic, Exams,
+                               ResultAnalysis)
+from assessment.utils import send_email_with_marks, get_option_alias
 from django.db.models.functions import Substr, Length
 from django.db.models import Value, Case, When, CharField
 from django.db.models.functions import Concat
@@ -27,6 +30,9 @@ class TopicListView(LoginRequiredMixin, ListView):
     model = Topic
     template_name = 'assessment/topic.html'
     context_object_name = "Topics"
+    
+    def get_queryset(self) -> QuerySet[Any]:
+        return super().get_queryset()
 
 
 class SubTopicListView(LoginRequiredMixin, ListView):
@@ -175,7 +181,6 @@ class AddQuestionView(LoginRequiredMixin, View):
                 # return reverse of add-question with assessemnt id
                 return HttpResponseRedirect(reverse('add-question', args=[assessment_id]))
             else:
-                print(form.errors)
                 messages.error(request, 'Question add failed.')
                 return HttpResponseRedirect(reverse('index'))
         else:
@@ -435,17 +440,50 @@ class TestShow(View):
 
     def post(self, request, *args, **kwargs):
         queryset = self.queryset.filter(assessment=self.kwargs['pk'])
-        count = 0
-        print(request.POST)
+        
+        total_attempted = len(request.POST)-1
+        correct_ans = 0
+        questions_status = {}
         for i in queryset:
+            questions_status[i.id] = get_option_alias(request.POST.get(str(i.id), None),i)
             if i.answer == request.POST.get(str(i.id), None):
-                count += 1
-        percentage = count/len(queryset)*100
-        return render(request, 'assessment/result.html', {'id': kwargs['pk'], 'percentage': percentage})
+                correct_ans += 1
+        
+        result = ResultAnalysis.objects.create(
+            user=request.user, assessment=Assessment.objects.get(id=self.kwargs['pk']),
+            attempted=total_attempted, correct=correct_ans,
+            questions_status=questions_status
+        )
+        
+        percentage = correct_ans/len(queryset)*100
+        return redirect(reverse('result', kwargs={'pk': result.id}))
 
 
-def result(request, pk, count):
-    return render(request, 'assessment/result.html', {'id':pk,'count':count})
+def result(request, pk):
+    result = ResultAnalysis.objects.get(id=pk)
+    answer_key_url = reverse('answer-key', kwargs={'pk': pk})
+    context = {
+        'result': result,
+        'answer_key_url':answer_key_url,
+    }
+    questions = Question.objects.filter(assessment=result.assessment.id)
+    question_data = []
+    for i in questions:
+        question_data.append({
+            'id': i.id,
+            'question': f'{i.question[:25]+"..." if len(i.question) > 25 else i.question}',
+            'option1': i.option1,
+            'option2': i.option2,
+            'option3': i.option3,
+            'option4': i.option4,
+            'correct_answer_alias': i.correct_answer_alias,
+            'explanation': i.explanation,
+            'correct_or_not': result.questions_status[str(i.id)]==i.correct_answer_alias,
+            'answer_given': result.questions_status[str(i.id)]
+        })
+        
+    context['question_data'] = question_data
+    return render(request, 'assessment/result.html', context)
 
 
 class ExamsListView(ListView):
@@ -490,3 +528,53 @@ def search_elasticsearch(request):
         "assessment": assessment_result
     }
     return JsonResponse({"results": results})
+
+def get_latest_5_results(request):
+    results = ResultAnalysis.objects.filter(user = request.user).order_by('-created_at')
+    if results:
+        if len(results) > 5:
+            results = results[:5]
+        else:
+            results = results
+    data = []
+    for result in results[::-1]:
+        data.append({
+            'created_at': result.formatted_created_at,
+            'percentage': result.percentage
+        })
+    
+    return JsonResponse(data, safe=False)
+
+
+def answer_key_page(request, pk):
+    result = ResultAnalysis.objects.get(id=pk)
+    context = {
+        'result': result
+    }
+    questions = Question.objects.filter(assessment=result.assessment.id)
+    question_data = []
+    for i in questions:
+        question_data.append({
+            'id': i.id,
+            'question': i.question,
+            'option1': i.option1,
+            'option2': i.option2,
+            'option3': i.option3,
+            'option4': i.option4,
+            'correct_answer_alias': i.correct_answer_alias,
+            'explanation': i.explanation,
+            'correct_or_not': result.questions_status[str(i.id)]==i.correct_answer_alias,
+            'answer_given': result.questions_status[str(i.id)]
+        })
+        
+    context['question_data'] = question_data
+    return render(request, 'assessment/answer_key.html', context)
+
+
+class ResultListView(ListView):
+    model = ResultAnalysis
+    template_name = 'assessment/result_list.html'
+    context_object_name = 'results'
+    
+    def get_queryset(self) -> QuerySet[Any]:
+        return super().get_queryset().filter(user=self.request.user)
