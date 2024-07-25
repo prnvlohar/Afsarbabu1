487213e1105c2d3a7ae50f8de0f9f156305df627
pranavlohar19 <144672776+pranavlohar19@users.noreply.github.com>
2024-07-25 13:06:17 +0530

    added changes for register-issue (#23)
    Co-authored-by: prnvlohar <prnvlohar@gmail.com>

--- assessment/utils.py
+++ assessment/utils.py
@@ -1,6 +1,8 @@
 
 from django.conf import settings
 from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
+import threading
 
 
 def send_email_with_marks(request, score):
@@ -21,3 +23,20 @@ def get_option_alias(option, question):
         return 'C'
     elif option == question.option4:
         return 'D'
+    
+
+def send_mail(subject, plain_text, html_content, to_mail):
+    message = EmailMultiAlternatives(
+        subject=subject, body=plain_text, to=[to_mail]
+    )
+    message.attach_alternative(html_content, "text/html")
+    message.send()
+
+
+def send_mail_tread(subject, plain_text, html_content, to_mail):
+    t = threading.Thread(
+        target=send_mail, args=(
+            subject, plain_text, html_content, to_mail
+        )
+    )
+    t.start()
